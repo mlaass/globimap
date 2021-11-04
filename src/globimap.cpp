f72333f919eb334401a2b1736716cadec8772e58
Moritz Laass <moritz.laass@tum.de>
2021-11-04 23:49:19 +0100

    ported globimaps to pybind11

--- src/globimap.cpp
+++ src/globimap.cpp
@@ -1,116 +1,101 @@
-/*
-This is the Python Interface based on Boost::Python and Boost::Numpy
-
-
-*/
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include<iostream>
 
-#include<boost/python.hpp>
-#include<boost/python/numpy.hpp>
-#include<boost/python/numpy/ndarray.hpp>
-#include <numpy/ndarrayobject.h> 
-#include <numpy/ndarraytypes.h> 
+#include <iostream>
+#include <functional>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
-// This holds the actual implementation. Copy this header to your projects (and a hasher, for example murmur.hpp)
+namespace py = pybind11;
+
+// This holds the actual implementation. Copy this header to your projects (and
+// a hasher, for example murmur.hpp)
+
 #include "globimap.hpp"
 
-
-namespace bp = boost::python;
-namespace np = boost::python::numpy;
-
-// Wrap a 1D C++ array (given as a pointer) to a numpy object.
-static boost::python::object wrap(double* data, npy_intp size) {
-  using namespace boost::python;
-
-  npy_intp shape[1] = { size }; // array size
-  PyObject* obj = PyArray_New(&PyArray_Type, 1, shape, NPY_DOUBLE, // data type
-                              NULL, data, 
-                              0, NPY_ARRAY_CARRAY, // NPY_ARRAY_CARRAY_RO for readonly
-                              NULL);
-  handle<> array( obj );
-  return object(array);
-}
-
 // Wrap 2D C++ array (given as pointer) to a numpy object.
-static boost::python::object wrap2D(double* data, npy_intp h, npy_intp w) {
-  using namespace boost::python;
+static py::array_t<double> wrap2D(double *data, size_t h, size_t w) {
 
-  npy_intp shape[2] = { h,w }; // array size
-  PyObject* obj = PyArray_New(&PyArray_Type, 2, shape, NPY_DOUBLE, // data type
-                              NULL, data, // data pointer
-                              0, NPY_ARRAY_CARRAY, // NPY_ARRAY_CARRAY_RO for readonly
-                              NULL);
-  handle<> array( obj );
-  return object(array);
+  auto shape = {h, w};
+  auto strides = std::vector<size_t>({w * 8, 8});
+  auto caps =
+      py::capsule(data, [](void *v) { /*delete reinterpret_cast<double *>(v);*/ });
+
+  return py::array_t<double, py::array::forcecast | py::array::c_style>(
+      shape, strides, data, caps);
 }
 
+// This template is a handy tool to call a function f(i,j,value) for each entry
+// of a 2D matrix self. template<typename func> static void
+void map_matrix(const py::array_t<double> &self, std::function<void(int,int,double)> f) {
+  if (self.ndim() != 2)
+    throw(std::runtime_error("2D array expected"));
+  auto s1 = self.strides(0);
+  auto s2 = self.strides(1);
+  const char * data = reinterpret_cast<const char *>(self.data());
 
-// This template is a handy tool to call a function f(i,j,value) for each entry of a 2D matrix self.
-template<typename func>
-static void map_matrix(np::ndarray &self, func f)
-{
-   auto nd = self.get_nd();
-   if (nd != 2) throw(std::runtime_error("2D array expected"));
-   auto s1 = self.strides(0);
-   auto s2 = self.strides(1);
-   auto data = self.get_data();
-
-   for (int i1=0; i1 < self.shape(0); i1++)
-   {
-     for(int i2=0; i2 < self.shape(1); i2++)
-     {
-         auto offset = i1 * s1 + i2 * s2;
-	 double *d = reinterpret_cast<double *> (data + offset);
-	 f(i1,i2,*d);
-     }
-   }
-   
-
+  for (int i1 = 0; i1 < self.shape(0); i1++) {
+    for (int i2 = 0; i2 < self.shape(1); i2++) {
+      size_t offset = i1 * s1 + i2 * s2;
+      //std::cout <<"("<< offset<<", "<<i1 <<", "<<i2 <<"), ";
+      const double *d = reinterpret_cast<const double *>(data + offset);
+      f(i1, i2, *d);
+    }
+  }
+  std::cout << std::endl;
 }
 
 // This wil be our implementation in C++ of a Python class globimap.
-typedef GloBiMap<bool> globimap_t ;
+typedef GloBiMap<bool> globimap_t;
 
 // The module begins
-BOOST_PYTHON_MODULE(globimap) {
-    import_array();
-    np::initialize();
-    using namespace boost::python;
-
-
-    // It exports a class (named globimap) with chained functions, see README.md
-   class_<globimap_t>("globimap")      
-       .def("rasterize", +[](globimap_t& self, size_t x, size_t y, size_t s0, size_t s1) -> object {
-	  auto &data = self.rasterize(x,y,s0,s1);
-          return wrap2D(&data[0],s0,s1);
-	})
-       .def("correct", +[](globimap_t& self, size_t x, size_t y, size_t s0, size_t s1) -> object {
-	  //auto &data = self.rasterize(x,y,s0,s1);
-	  auto &data = self.apply_correction(x,y,s0,s1);	  
-          return wrap2D(&data[0],s0,s1);
-	})
-       .def("put", +[](globimap_t &self, uint32_t x, uint32_t y) { self.put({x,y});})
-       .def("get", +[](globimap_t &self, uint32_t x, uint32_t y) -> bool { return self.get({x,y});})
-       .def("configure", +[](globimap_t &self, size_t k, size_t m)  {  self.configure(k,m);})
-       .def("clear", +[](globimap_t &self){self.clear();})
-       .def("summary", +[](globimap_t &self)->std::string { return self.summary();})
-       .def("map", +[](globimap_t &self,object mat, int o0, int o1) {
-		np::ndarray a = np::from_object(mat, np::dtype::get_builtin<double>());
-		map_matrix(a, [&](int i0, int i1, double v){
-		if (v != 0 && v!= 1)
-		 throw(std::runtime_error("data is not binary."));
-		if (v == 1)
-   		    self.put({static_cast<uint32_t>(o0+i0),static_cast<uint32_t>(o1+i1)});
-		});
-	})
-	.def("enforce",+[](globimap_t &self, object mat, int o0, int o1){
-		np::ndarray a = np::from_object(mat, np::dtype::get_builtin<double>());
-		map_matrix(a, [&](int i0, int i1, double v){
-		if (v==0 && self.get({static_cast<uint32_t>(o0+i0),static_cast<uint32_t>(o1+i1)}))
-		{// this is a false positive
-		    self.add_error({static_cast<uint32_t>(o0+i0),static_cast<uint32_t>(o1+i1)});
-		}
-		});
-	});
+PYBIND11_MODULE(globimap, m) {
+  // It exports a class (named globimap) with chained functions, see README.md
+  py::class_<globimap_t>(m, "globimap")
+      .def(py::init<>())
+      .def("rasterize",
+           +[](globimap_t &self, size_t x, size_t y, size_t s0, size_t s1) {
+             auto &data = self.rasterize(x, y, s0, s1);
+             return wrap2D(&data[0], s0, s1);
+           })
+      .def("correct",
+           +[](globimap_t &self, size_t x, size_t y, size_t s0,
+               size_t s1) -> py::array {
+             auto &data = self.apply_correction(x, y, s0, s1);
+             return wrap2D(&data[0], s0, s1);
+           })
+      .def("put",
+           +[](globimap_t &self, uint32_t x, uint32_t y) {
+             self.put({x, y});
+           })
+      .def("get",
+           +[](globimap_t &self, uint32_t x, uint32_t y) -> bool {
+             return self.get({x, y});
+           })
+      .def("configure",
+           +[](globimap_t &self, size_t k, size_t m) { self.configure(k, m); })
+      .def("clear", +[](globimap_t &self) { self.clear(); })
+      .def("summary",
+           +[](globimap_t &self) -> std::string { return self.summary(); })
+      .def("map", +[](globimap_t &self, py::array mat, int o0, int o1) {
+          map_matrix(mat, [&](int i0, int i1, double v) {
+            if (v != 0 && v != 1){
+              std::cout << v<< std::endl;
+              throw(std::runtime_error("data is not binary."));
+            }
+              
+            if (v == 1)
+              self.put({static_cast<uint32_t>(o0 + i0),
+                        static_cast<uint32_t>(o1 + i1)});
+          });
+        })
+      .def("enforce", +[](globimap_t &self, py::array mat, int o0, int o1) {
+        map_matrix(mat, [&](int i0, int i1, double v) {
+          if (v == 0 && self.get({static_cast<uint32_t>(o0 + i0),
+                                  static_cast<uint32_t>(o1 + i1)})) {
+            // this is a false positive
+            self.add_error({static_cast<uint32_t>(o0 + i0),
+                            static_cast<uint32_t>(o1 + i1)});
+          }
+        });
+      });
 }
--- src/sample.py
+++ src/sample.py
@@ -1,56 +1,57 @@
-import numpy as np;
-import globimap as gm;
-from matplotlib import pyplot as plt;
-from tqdm import tqdm;
+import numpy as np
+import globimap as gm
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
 
 class cfg:
-    k=15
-    logm=23
-    low=[47,11] # where to put this into infinite space
+    k = 15
+    logm = 23
+    low = [47, 11]  # where to put this into infinite space
 
 
-def generate_sierpinski (size=4096, steps=500000):
-    m = np.zeros(size*size).reshape(size,size);
+def generate_sierpinski(size=4096, steps=50000):
+    m = np.zeros(size*size).reshape(size, size)
     print(m.shape)
-    triangle = [[0,0],[1,0],[0.5,1]];
+    triangle = [[0, 0], [1, 0], [0.5, 1]]
     s = np.random.rand(2)
     for _ in tqdm(range(steps)):
-        p = triangle[np.random.choice(range(3))] 
+        p = triangle[np.random.choice(range(3))]
         s = s + (p-s)/2
         c = (s * size).astype(np.uint64)
-        m[c[0],c[1]] = 1
+        m[c[0], c[1]] = 1
     return (m)
-   
-    
-    
 
-if __name__=="__main__":
-    print("Memory: %f KB" % (2**cfg.logm / 8.0 / 1024.0 ))
+
+if __name__ == "__main__":
+    print("Memory: %f KB" % (2**cfg.logm / 8.0 / 1024.0))
     print("Hashes: %d " % (cfg.k))
-    
+
     patch = generate_sierpinski()
     plt.subplot(131)
     plt.imshow(patch)
-    print ("Ones: %d" % (np.sum(patch==1)))
-    print ("Coordinate Memory (4 bytes): %d" %(np.sum(patch==1)*4))
-    print ("Raster Memory (1 bit per pixel): %d KB" %(4096*4096/8/1024))
+    print("Ones: %d" % (np.sum(patch == 1)))
+    print("Zeros: %d" % (np.sum(patch == 0)))
+    print("other: %d" % (np.sum((patch != 0) & (patch != 1))))
+    print("Coordinate Memory (4 bytes): %d" % (np.sum(patch == 1)*4))
+    print("Raster Memory (1 bit per pixel): %d KB" % (4096*4096/8/1024))
 
-    
     m = gm.globimap()
-    m.configure(cfg.k,cfg.logm)
+    m.configure(cfg.k, cfg.logm)
     print("Step 1: Probabilistic Encoding")
-    m.map(patch, cfg.low[0],cfg.low[1])
+    m.map(patch, cfg.low[0], cfg.low[1])
 
     # now add the error correction information
     print("Step 2: Error Correction Information")
-    m.enforce(patch, cfg.low[0],cfg.low[1])
-    print(m.summary());
+    m.enforce(patch, cfg.low[0], cfg.low[1])
+    print(m.summary())
 
     print("Step 3: Rasterize")
-    result = m.rasterize(cfg.low[0], cfg.low[1], patch.shape[0], patch.shape[1])
+    result = m.rasterize(cfg.low[0], cfg.low[1],
+                         patch.shape[0], patch.shape[1])
     plt.subplot(132)
     plt.imshow(result)
-    
+
     result = m.correct(cfg.low[0], cfg.low[1], patch.shape[0], patch.shape[1])
     plt.subplot(133)
     plt.imshow(result)
@@ -58,4 +59,3 @@ if __name__=="__main__":
     nerror = np.sum(result != patch)
     ber = float(nerror) / np.prod(patch.shape)
     print("Have %d errors for a BER of %f" % (nerror, ber))
-
