740553328816025b9ad5f4f13084dff03cc6a79c
Moritz Laass <moritz.laass@tum.de>
2022-02-27 02:51:17 +0100

    experiments almost done

---
+++ globimap/globimap_print_poly_stats.cpp
@@ -0,0 +1,138 @@
+#include "globimap_test_config.hpp"
+#include "globimap_v2.hpp"
+#include <algorithm>
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <math.h>
+#include <string>
+
+#include <highfive/H5File.hpp>
+#include <tqdm.hpp>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+#include "archive.h"
+#include "loc.hpp"
+#include "rasterizer.hpp"
+#include "shapefile.hpp"
+
+#include <H5Cpp.h>
+
+namespace fs = std::filesystem;
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+namespace bgt = boost::geometry::strategy::transform;
+typedef bg::model::point<double, 2, bg::cs::cartesian> point_t;
+typedef bg::model::box<point_t> box_t;
+typedef bg::model::polygon<point_t> polygon_t;
+
+typedef std::vector<polygon_t> poly_collection_t;
+
+#ifndef TUM1_ICAML_ORG
+const std::string base_path = "/mnt/G/datasets/atlas/";
+const std::string vector_base_path = "/mnt/G/datasets/vector/";
+const std::string experiments_path =
+    "/home/moritz/workspace/bgdm/globimap/experiments/";
+#else
+const std::string base_path = "/home/moritz/tf/pointclouds_2d/data/";
+const std::string experiments_path = "/home/moritz/tf/globimap/experiments/";
+const std::string vector_base_path = "/home/moritz/tf/vector/";
+#endif
+
+std::vector<std::string> datasets{"twitter_1mio_coords.h5",
+                                  "twitter_10mio_coords.h5"};
+
+// std::vector<std::string> datasets{"twitter_200mio_coords.h5",
+//                                   "asia_200mio_coords.h5"};
+std::vector<std::string> polygon_sets{"tl_2017_us_zcta510",
+                                      "Global_LSIB_Polygons_Detailed"};
+template <typename T> std::string render_hist(std::vector<T> hist) {
+  std::stringstream ss;
+  ss << "[";
+  for (auto i = 0; i < hist.size(); ++i) {
+    ss << hist[i] << ((i < (hist.size() - 1)) ? ", " : "");
+  }
+  ss << "]";
+  return ss.str();
+}
+
+template <typename T>
+std::string render_stat(const std::string &name, std::vector<T> stat) {
+  double stat_min = FLT_MAX;
+  double stat_max = 0;
+  double stat_mean = 0;
+  double stat_std = 0;
+
+  for (double v : stat) {
+    stat_min = std::min(stat_min, v);
+    stat_max = std::max(stat_max, v);
+    stat_mean += v;
+  }
+  stat_mean /= stat.size();
+
+  for (double v : stat) {
+    stat_std += pow((stat_mean - v), 2);
+  }
+  stat_std /= stat.size();
+  stat_std = sqrt(stat_std);
+
+  std::stringstream ss;
+  ss << "\"" << name << "\": {\n";
+  ss << "\"min\": " << stat_min << ",\n";
+  ss << "\"max\": " << stat_max << ",\n";
+  ss << "\"mean\": " << stat_mean << ",\n";
+  ss << "\"std\": " << stat_std << ",\n";
+  ss << "\"hist\": " << render_hist(globimap::make_histogram(stat, 1000))
+     << "\n";
+  ss << "}";
+  return ss.str();
+}
+
+int main() {
+  uint width = 2 * 8192, height = 2 * 8192;
+  std::string exp_name = "polygons_stat";
+  mkdir((experiments_path + exp_name).c_str(), 0777);
+  for (auto shp : polygon_sets) {
+    std::stringstream ss1;
+    ss1 << shp << "-" << width << "x" << height;
+    auto polyset_name = ss1.str();
+    std::stringstream ss;
+    ss << vector_base_path << polyset_name;
+    auto poly_path = ss.str();
+
+    int poly_count = 0;
+    for (auto e : fs::directory_iterator(poly_path)) {
+      poly_count += (e.is_regular_file() ? 1 : 0);
+    }
+    std::vector<uint64_t> polysizes;
+    for (auto idx = 0; idx < poly_count; idx++) {
+      std::vector<uint64_t> raster;
+      std::stringstream ss;
+      ss << poly_path << "/" << std::setw(8) << std::setfill('0') << idx;
+      auto filename = ss.str();
+      std::ifstream ifile(filename, std::ios::binary);
+      if (!ifile.is_open()) {
+        std::cout << "ERROR file doesn't exist: " << filename << std::endl;
+      }
+      Archive<std::ifstream> a(ifile);
+      a >> raster;
+      polysizes.push_back(raster.size());
+      ifile.close();
+    }
+    std::stringstream fss;
+    fss << experiments_path << exp_name << "/" << polyset_name << ".json";
+    std::ofstream out(fss.str());
+    out << "{" << render_stat("polysizes", polysizes) << ",\n";
+    out << "\"size\": " << poly_count << "" << std::endl;
+    out << "}" << std::endl;
+
+    out.close();
+    std::cout << "end: " << shp << std::endl;
+  }
+};
---
+++ globimap/globimap_test_dataset_full_time.cpp
@@ -0,0 +1,152 @@
+#include "globimap_v2.hpp"
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <string>
+
+#include <highfive/H5File.hpp>
+#include <tqdm.hpp>
+#include <tqdm/tqdm.h>
+
+#include "globimap_test_config.hpp"
+
+const std::string base_path = "/home/moritz/tf/pointclouds_2d/data/";
+const std::string experiments_path = "/home/moritz/tf/globimap/experiments/";
+
+std::vector<std::string> datasets{"twitter_coords.h5", "asia-latest.h5"};
+
+static std::string test_encode(globimap::Globimap<> &g, const std::string &name,
+                               const std::string &ds, uint width, uint height,
+                               bool errord) {
+  auto filename = base_path + ds;
+  auto batch_size = 4096;
+  std::cout << "Start test_h5 encode with {fn: \"" << filename
+            << "\" } for: " << name << std::endl;
+  using namespace HighFive;
+
+  // we create a new hdf5 file
+  auto file = File(filename, File::ReadWrite);
+  std::vector<std::vector<double>> read_data;
+
+  // we get the dataset
+  DataSet dataset = file.getDataSet("coords");
+
+  using std::chrono::duration;
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
+  auto t1 = high_resolution_clock::now();
+
+  std::vector<std::vector<double>> result;
+  auto shape = dataset.getDimensions();
+  int batches = std::floor(shape[0] / batch_size);
+  std::cout << "Start test_h5 encode with {fn: \"" << filename
+            << "\" } for: " << name << "\nbatches: " << batches
+            << "\nbatchsize: " << batch_size << std::endl;
+  for (auto i : tq::trange(batches)) {
+    dataset.select({i * batch_size, 0}, {batch_size, 2}).read(result);
+
+    for (auto p : result) {
+      double x = (double)width * (((double)p[0] + 180.0) / 360.0);
+      double y = (double)height * (((double)p[0] + 90.0) / 180.0);
+      g.put({(uint64_t)x, (uint64_t)y});
+    }
+  }
+  auto t2 = high_resolution_clock::now();
+  duration<double, std::milli> insert_time = t2 - t1;
+  std::stringstream ss;
+
+  if (errord) {
+
+    auto t3 = high_resolution_clock::now();
+    g.detect_errors(0, 0, width, height);
+    auto t4 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> errord_time = t4 - t3;
+
+    ss << "{\"summary\":" << g.summary() << ",\n";
+    ss << "\"insert_time\": " << insert_time.count() / 1000.0 << ",\n";
+    ss << "\"errord_time\": " << errord_time.count() / 1000.0 << "}"
+       << std::endl;
+  } else {
+
+    uint num_queries = 1000;
+    std::vector<double> times;
+    double query_time_mean = 0;
+    double query_time_min = MAXFLOAT;
+    double query_time_max = 0;
+    for (auto j = 0; j < 10; j++) {
+
+      auto t5 = high_resolution_clock::now();
+      for (auto i = 0; i < num_queries; ++i) {
+        uint64_t x = rand() % width;
+        uint64_t y = rand() % height;
+        g.get_min({x, y});
+      }
+      auto t6 = high_resolution_clock::now();
+
+      duration<double, std::milli> query_time = t6 - t5;
+      double qt = query_time.count() / 1000.0;
+      query_time_mean += qt;
+      times.push_back(qt);
+      query_time_min = std::min(qt, query_time_min);
+      query_time_max = std::max(qt, query_time_max);
+    }
+    query_time_mean /= 10.0;
+
+    std::stringstream tss;
+    tss << "[";
+    for (auto it = 0; it < times.size(); it++) {
+      tss << times[it] << ((it == times.size() - 1) ? "" : ", ");
+    }
+    tss << "]";
+
+    ss << "{\"summary\":" << g.summary() << ",\n";
+    ss << "\"perf\": {\"query_time\":" << query_time_mean << ",\n";
+    ss << "\"query_time_min\":" << query_time_min << ",\n";
+    ss << "\"query_time_max\":" << query_time_max << ",\n";
+    ss << "\"times\":" << tss.str() << ",\n";
+    ss << "\"num_queries\":" << num_queries << ",\n";
+    ss << "\"insert_time\": " << insert_time.count() / 1000.0 << "\n}}"
+       << std::endl;
+  }
+  return ss.str();
+}
+
+int main() {
+  config_t cfgs;
+  get_configurations(cfgs, {16, 20, 24}, {8, 16, 32});
+
+  {
+    uint k = 8;
+    auto x = 0;
+    uint width = 8192, height = 8192;
+    std::string exp_name = "test_datasets_full_time";
+    save_configs(experiments_path + std::string("config_") + exp_name, cfgs);
+    mkdir((experiments_path + exp_name).c_str(), 0777);
+    for (auto c : cfgs) {
+      globimap::FilterConfig fc{k, c};
+      std::cout << "fc: " << fc.to_string() << std::endl;
+      auto y = 0;
+      for (auto d : datasets) {
+        std::stringstream fss;
+        fss << experiments_path << exp_name << "/" << exp_name << ".w" << width
+            << "h" << height << "."
+            << "-" << fc.to_string() << d << ".json";
+        if (file_exists(fss.str())) {
+          std::cout << "file already exists: " << fss.str() << std::endl;
+        } else {
+          std::cout << "run: " << fss.str() << std::endl;
+          std::ofstream out(fss.str());
+          auto g = globimap::Globimap(fc, false);
+          out << test_encode(g, fc.to_string(), d, width, height, false);
+          out.close();
+        }
+        y++;
+      }
+      x++;
+    }
+  }
+};
